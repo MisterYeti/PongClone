1a6bca1c1e123ffea6d0b93f15bdcd90a4d03f2b
Thomas Biffaud <tbiffaud@gmail.com>
2023-03-01 22:58:02 +0100

    Init repo + base gameplay

---
+++ Assets/_Scripts/Game.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField, Min(0f)]
+    Vector2 arenaExtents = new Vector2(10f, 10f);
+
+    [SerializeField]
+    Ball ball;
+
+    [SerializeField]
+    Paddle bottomPaddle, topPaddle;
+
+    private void Awake()
+    {
+        ball.StartNewGame();
+    }
+
+    private void Update()
+    {
+        bottomPaddle.Move(ball.Position.x, arenaExtents.x);
+        topPaddle.Move(ball.Position.x, arenaExtents.x);
+        ball.Move();
+        BounceYIfNeeded();
+        BounceXIfNeeded(ball.Position.x);
+        ball.UpdateVisualization();
+    }
+
+    private void BounceYIfNeeded()
+    {
+        float yExtents = arenaExtents.y - ball.Extents;
+        if (ball.Position.y < -yExtents)
+        {
+            BounceY(-yExtents, bottomPaddle);
+        }
+        else if (ball.Position.y > yExtents)
+        {
+            BounceY(yExtents, topPaddle);
+        }
+    }
+    private void BounceXIfNeeded(float x)
+    {
+        float xExtents = arenaExtents.x - ball.Extents;
+        if (x < -xExtents)
+        {
+            ball.BounceX(-xExtents);
+        }
+        else if (x > xExtents)
+        {
+            ball.BounceX(xExtents);
+        }
+    }
+
+    private void BounceY(float boundary, Paddle defender)
+    {
+        float durationAfterBounce = (ball.Position.y - boundary) / ball.Velocity.y;
+        float bounceX = ball.Position.x - ball.Velocity.x * durationAfterBounce;
+        BounceXIfNeeded(bounceX);
+        bounceX = ball.Position.x - ball.Velocity.x * durationAfterBounce;
+        ball.BounceY(boundary);
+
+        if (defender.HitBall(bounceX, ball.Extents, out float hitFactor))
+        {
+            ball.SetXPositionAndSpeed(bounceX, hitFactor, durationAfterBounce);
+        }
+    }
+}
